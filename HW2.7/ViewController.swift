77c8033e77bfdf9b061da3da189943a038c85624
VGO <vgorobets@yandex.ru>
2020-09-08 11:08:08 +0300

    Initial Commit

---
+++ HW2.7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW2.7
+//
+//  Created by VICTOR on 08.09.2020.
+//  Copyright © 2020 VICTOR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
